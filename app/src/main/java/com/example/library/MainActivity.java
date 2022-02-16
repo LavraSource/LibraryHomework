d2193a3862513d42e98459402348507d11fba253
Lavra Source <lavriket@gmail.com>
2022-02-16 21:12:13 +0800

    hi

--- app/src/main/java/com/example/library/MainActivity.java
+++ app/src/main/java/com/example/library/MainActivity.java
@@ -1,21 +1,16 @@
 package com.example.library;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDialogFragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -26,11 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button add,del;
 
-    EditText name,author,year;
-
-
-
-
+    EditText bookName,bookAuthor, bookYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,20 +33,20 @@ public class MainActivity extends AppCompatActivity {
         add=findViewById(R.id.add);
         del=findViewById(R.id.del);
 
-        name=findViewById(R.id.name);
-        author=findViewById(R.id.author);
-        year=findViewById(R.id.year);
+        bookName =findViewById(R.id.name);
+        bookAuthor=findViewById(R.id.author);
+        bookYear =findViewById(R.id.year);
 
 
 
 
         //TODO подготовка данных
         LinkedList<Book> bookLinkedList=new LinkedList<>();
-        bookLinkedList.add(new Book("Основание","АюАзимов",2015,R.drawable.osnovanie));
-        bookLinkedList.add(new Book("Преступление и наказание","Достаевский",1972,R.drawable.prestuplenie));
+        bookLinkedList.add(new Book("Основание","Азимов",2015,R.drawable.osnovanie));
+        bookLinkedList.add(new Book("Преступление и наказание","Достоевский",1972,R.drawable.prestuplenie));
         bookLinkedList.add(new Book("Шинель","Гоголь",1998,R.drawable.shinel));
-        bookLinkedList.add(new Book("Роковые яйца","М.Булгаков",2018,R.drawable.book));
-        bookLinkedList.add(new Book("Колобок","народ",2001,R.drawable.book));
+        bookLinkedList.add(new Book("Гарри Поттер и Философский каамень","Дж Роулинг",2008,R.drawable.book));
+        bookLinkedList.add(new Book("Колобок","народ",2021,R.drawable.book));
 
 
         //TODO сщздать массив с ключами и идентификаторами
@@ -63,14 +54,14 @@ public class MainActivity extends AppCompatActivity {
         int [] idArray={R.id.book_title,R.id.author,R.id.year,R.id.image};
 
         //TODO сщздание списка map для адаптера
-        LinkedList<HashMap<String,Object>> listForAdopter=new LinkedList<>();
+        LinkedList<HashMap<String,Object>> listForAdapter=new LinkedList<>();
         for (int i = 0; i < bookLinkedList.size(); i++) {
             HashMap<String,Object>bookMap=new HashMap<>();
             bookMap.put(keyArray[0],bookLinkedList.get(i).title);
             bookMap.put(keyArray[1],bookLinkedList.get(i).author);
             bookMap.put(keyArray[2],bookLinkedList.get(i).year);
             bookMap.put(keyArray[3],bookLinkedList.get(i).coverId);
-            listForAdopter.add(bookMap);
+            listForAdapter.add(bookMap);
 
         }
         //TODO создпние адаптера
@@ -78,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         //ArrayAdapter<Book>adapter=new ArrayAdapter<>(this,R.layout.list_item,bookLinkedList);
         //SimpleAdapter adapter1;
         //SimpleCursorAdapter adapter2;
-        SimpleAdapter simpleAdapter=new SimpleAdapter(this,listForAdopter,R.layout.list_item,keyArray,idArray);
+        SimpleAdapter simpleAdapter=new SimpleAdapter(this,listForAdapter,R.layout.list_item,keyArray,idArray);
 
         bookList.setAdapter(simpleAdapter);
         bookList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -87,9 +78,8 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), bookLinkedList.get(i).toString(), Toast.LENGTH_SHORT).show();
                 FragmentManager manager = getSupportFragmentManager();
                 MyAlertDialog myDialogFragment = new MyAlertDialog();
-                //myDialogFragment.show(manager, "myDialog");
                 Bundle args = new Bundle();
-                String a=listForAdopter.get(i).get("title").toString()+listForAdopter.get(i).get("author").toString();
+                String a=listForAdapter.get(i).get("title").toString()+listForAdapter.get(i).get("author").toString();
                 args.putString("name",a );
                 myDialogFragment.setArguments(args);
                 FragmentTransaction transaction = manager.beginTransaction();
@@ -98,16 +88,15 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        //TODO создать интерфейс(можно кнопками) добавления/удаления кнниги
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name1=name.getText().toString();
-                String author1=author.getText().toString();
+                String name1= bookName.getText().toString();
+                String author1=bookAuthor.getText().toString();
                 int year1=0;
                 try {
-                    year1 = Integer.parseInt(year.getText().toString());
+                    year1 = Integer.parseInt(bookYear.getText().toString());
                 } catch (NumberFormatException e) {
                     Toast.makeText(getApplicationContext(), "введите год числом", Toast.LENGTH_SHORT).show();
 
@@ -119,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
                     bookMap.put(keyArray[1], author1);
                     bookMap.put(keyArray[2], year1);
                     bookMap.put(keyArray[3], R.drawable.book);
-                    listForAdopter.add(bookMap);
+                    listForAdapter.add(bookMap);
                     simpleAdapter.notifyDataSetChanged();
                 }
 
@@ -131,11 +120,11 @@ public class MainActivity extends AppCompatActivity {
         del.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name1=name.getText().toString();
-                String author1=author.getText().toString();
-                for (int i = 0; i < listForAdopter.size(); i++) {
+                String name1= bookName.getText().toString();
+                String author1=bookAuthor.getText().toString();
+                for (int i = 0; i < listForAdapter.size(); i++) {
                     if((name1+" "+author1).equals(bookLinkedList.get(i).toString())){
-                        listForAdopter.remove(i);
+                        listForAdapter.remove(i);
                         break;
                     }
                 }
@@ -144,8 +133,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        simpleAdapter.notifyDataSetChanged();//обновление экрана
-
-        //TODO сделать аннотацию к книге
+        simpleAdapter.notifyDataSetChanged();
     }
 }
